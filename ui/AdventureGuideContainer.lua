004fafb33661c281683da8e8806d81121486729f
TomCat <tomcat@tomcatstours.com>
2023-06-27 20:36:41 -0400

    Enable main window draggable and position saved account-wide

--- ui/AdventureGuideContainer.lua
+++ ui/AdventureGuideContainer.lua
@@ -11,9 +11,9 @@ local component = UI.CreateComponent("AdventureGuideContainer")
 function component.Init()
     -- Create the base window and portrait image
     component.frame = CreateFrame("Frame", addonName .. "_AdventureGuideContainer", UIParent, "PortraitFrameTemplate")
+    --_G[addonName .. "_AdventureGuideContainer"] = nil
     local frame = component.frame
     frame:SetSize(800, 496)
-    frame:SetPoint("CENTER")
     frame:EnableMouse(true)
     frame:SetToplevel(true)
     frame.title = _G[addonName .. "_AdventureGuideContainerTitleText"]
@@ -25,8 +25,29 @@ function component.Init()
     mask:SetTexture("Interface\\CharacterFrame\\TempPortraitAlphaMask", "CLAMPTOBLACKADDITIVE", "CLAMPTOBLACKADDITIVE")
     frame.portrait:AddMaskTexture(mask)
 
+    -- Allow the window to be moved with last position saved account-wide
+    if (SavedVariables.AdventureGuideContainerLocation) then
+        frame:ClearAllPoints()
+        frame:SetPoint(unpack(SavedVariables.AdventureGuideContainerLocation))
+    else
+        frame:SetPoint("CENTER")
+    end
+    frame:RegisterForDrag("LeftButton")
+    frame:SetClampedToScreen(true)
+    frame:SetScript("OnDragStart", function(self)
+        frame:SetMovable(true)
+        if not self.isLocked then
+            self:StartMoving()
+        end
+    end)
+    frame:SetScript("OnDragStop", function(self)
+        frame:SetMovable(false)
+        self:StopMovingOrSizing()
+        SavedVariables.AdventureGuideContainerLocation = { self:GetPoint() }
+    end)
+
     --todo: Show only via a slash command and UI button
     component.frame:Show()
 end
 
-    UI.Add(component)
+UI.Add(component)
