1dbf3f3aa1ca8620239680538fd974298730d858
TomCat <tomcat@tomcatstours.com>
2023-08-28 05:48:35 -0400

    Instance info overview button

--- ui/AdventureGuideInstanceInfo.lua
+++ ui/AdventureGuideInstanceInfo.lua
@@ -20,7 +20,8 @@ function component.Init(components_)
 	frame.info = info
 	info:SetSize(785, 425)
 	info:SetPoint("BOTTOMRIGHT", -1, 2)
-	info.bg = info:CreateTexture(nil, "BACKGROUND")
+	info.bg = info:CreateTexture()
+	info.bg:SetDrawLayer("BACKGROUND", 1)
 	info.bg:SetTexture("Interface/EncounterJournal/UI-EJ-JournalBG")
 	info.bg:SetTexCoord(0, 0.766601562, 0, 0.830078125)
 	info.bg:SetAllPoints()
@@ -53,6 +54,23 @@ function component.Init(components_)
 	info.instanceTitle:SetSize(290, 12)
 	info.instanceTitle:SetPoint("LEFT", info.difficultyIcon, "RIGHT", -4, 0)
 	info.instanceTitle:SetTextColor(0.902, 0.788, 0.671)
+	info.instanceButton = CreateFrame("Button", nil, info)
+	info.instanceButton:SetMotionScriptsWhileDisabled(true)
+	info.instanceButton:SetSize(64, 61)
+	info.instanceButton:SetPoint("TOPLEFT", 0, -3)
+	info.instanceButton.icon = info.instanceButton:CreateTexture()
+	info.instanceButton.icon:SetDrawLayer("BACKGROUND", 6)
+	info.instanceButton.icon:SetSize(64, 64)
+	info.instanceButton.icon:SetPoint("TOPLEFT", 6.5, -7)
+	local instanceButtonBorder = info.instanceButton:CreateTexture()
+	instanceButtonBorder:SetTexture("Interface/EncounterJournal/UI-EncounterJournalTextures")
+	instanceButtonBorder:SetTexCoord(0.50585938, 0.63085938, 0.02246094, 0.08203125)
+	info.instanceButton:SetNormalTexture(instanceButtonBorder)
+	local instanceButtonBorderHighlight = info.instanceButton:CreateTexture()
+	instanceButtonBorderHighlight:SetTexture("Interface/EncounterJournal/UI-EncounterJournalTextures")
+	instanceButtonBorderHighlight:SetTexCoord(0.50585938, 0.63085938, 0.02246094, 0.08203125)
+	info.instanceButton:SetHighlightTexture(instanceButtonBorderHighlight, "ADD")
+	--todo: set OnClick
 	frame:Hide()
 end
 
@@ -62,8 +80,14 @@ function component.ShowInstanceInfo(instanceID)
 		instance = RaidsByInstanceID[instanceID]
 	end
 	component.frame.info.instanceTitle:SetText(instance.name)
+	component.frame.info.instanceButton.icon:SetMask(I.InstanceButtonIconMask);
+	component.frame.info.instanceButton.icon:SetTexture(instance.buttonImage2)
+	components.AdventureGuideInstanceOverview.ShowInstanceOverview(instanceID)
 	components.AdventureGuideEncounters.SetInstance(instance)
 	components.AdventureGuideContainer.SetCurrentView(component.frame)
+	components.AdventureGuideNavBar.SetInstance(instance.instanceID)
+	components.AdventureGuideNavBar.SetEncounter()
+	components.AdventureGuideNavBar.Refresh()
 end
 
 UI.Add(component)
