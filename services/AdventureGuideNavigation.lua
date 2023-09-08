60d3c444908c979c5329fdd5e834d6db0a0b87f0
TomCat <tomcat@tomcatstours.com>
2023-09-08 02:16:37 -0400

    Added navigation service

---
+++ services/AdventureGuideNavigation.lua
@@ -0,0 +1,43 @@
+--[[
+Copyright (C) 2023 FooxyTV (simon@fooxy.tv)
+All rights reserved.
+
+Programming by: TomCat / TomCat's Gaming
+]]
+select(2, ...).SetupGlobalFacade()
+
+local instance
+local instances
+local encounter
+
+AdventureGuideNavigationService = { }
+
+function AdventureGuideNavigationService.Reset()
+	instance = nil
+	instances = nil
+	encounter = nil
+end
+
+function AdventureGuideNavigationService.GetEncounter()
+	return encounter
+end
+
+function AdventureGuideNavigationService.GetInstance()
+	return instance
+end
+
+function AdventureGuideNavigationService.GetInstances()
+	return instances
+end
+
+function AdventureGuideNavigationService.SetEncounter(ref)
+	encounter = ref
+end
+
+function AdventureGuideNavigationService.SetInstance(ref)
+	instance = ref
+end
+
+function AdventureGuideNavigationService.SetInstances(ref)
+	instances = ref
+end
