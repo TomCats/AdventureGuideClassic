5810e76123b5163a7fe4c1fd48f9547dbaa04730
TomCat <tomcat@tomcatstours.com>
2023-09-07 22:43:05 -0400

    Updated slash command to support minimap button enable/disable

--- SlashCommands.lua
+++ SlashCommands.lua
@@ -6,5 +6,14 @@ Programming by: TomCat / TomCat's Gaming
 ]]
 select(2, ...).SetupGlobalFacade()
 
-_G.SLASH_ADVENTUREGUIDECLASSIC1 = "/ag"
-SlashCmdList["ADVENTUREGUIDECLASSIC"] = UI.ToggleEncounterJournal
+_G.SLASH_ADVENTUREGUIDECLASSIC1 = "/agc"
+SlashCmdList["ADVENTUREGUIDECLASSIC"] = function(message)
+	message = string.lower(message)
+	if (message == "") then
+		UI.ToggleEncounterJournal()
+	elseif (message == "button") then
+		MinimapButton.Toggle()
+	else
+		--todo: print usage instructions to the console
+	end
+end
